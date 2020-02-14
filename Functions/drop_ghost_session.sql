494eb98e315af6e77cda6e0935b68f72d723e5c4
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2020-02-14 10:11:42 +0300

    Create drop_ghost_session.sql

---
+++ Functions/drop_ghost_session.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE FUNCTION SYS.DROP_USER_DMP
+RETURN NUMBER
+AS 
+PRAGMA AUTONOMOUS_TRANSACTION;
+
+CURSOR c_get_cursor IS
+
+SELECT    'alter system kill session '''
+                 || s.sid
+                 || ','
+                 || s.serial#
+                 || ''';' AS DROP_SESSION
+            FROM v$session s, v$process p
+           WHERE     s.username IN ('XXX', 'XXX', 'XXX')
+                 AND p.addr(+) = s.paddr;
+                 
+ BEGIN
+    BEGIN
+       FOR object_rec IN c_get_cursor
+        LOOP
+            EXECUTE IMMEDIATE
+                ( object_rec.DROP_SESSION );
+                END LOOP;
+                END;
+                RETURN 0;
+                END DROP_USER_DMP;
+                /
